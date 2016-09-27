c9a2681a01fcbb7848cf1f983fcb7d83d609639c
Sogin George <845178snge@TCS-MOBILITY.COM>
2016-09-27 15:12:46 +0530

    First commit

---
+++ src/main/java/com/tcs/weatherdata/App.java
@@ -0,0 +1,13 @@
+package com.tcs.weatherdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
